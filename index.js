45dc25b4b7bfab8a5452d97ee4b9ae0a46b12231
Daria Makarenko <darya.makarenko@gmail.com>
2024-06-02 20:39:52 +0300

    changes to name conventions

--- index.js
+++ index.js
@@ -4,25 +4,72 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-//Step 1: Run the solution.js file without looking at the code.
-//Step 2: You can go to the recipe.json file to see the full structure of the recipeJSON below.
-const recipeJSON =
-  '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
+const writersJSON = [
+  {
+    id: "0001",
+    name: "Viktoria Amelina",
+    about: {
+      name: "Viktoria",
+      text1:
+        "Ukrainian novelist who became a war crimes investigator following the Russian invasion, and fought to uncover the true stories of her compatriots’ experiences under occupation",
+      text2: "some text2",
+      text3: "some text3",
+    },
+  },
+  {
+    id: "0002",
+    name: "Volodymyr Vakulenko",
+    ingredients: {
+      about: {
+        name: "Volodymyr",
+        text1:
+          "Ukrainian novelist who became a war crimes investigator following the Russian invasion, and fought to uncover the true stories of her compatriots’ experiences under occupation",
+        text2: "some text2",
+        text3: "some text3",
+      },
+    },
+  },
+  {
+    id: "0003",
+    name: "Maksym Krivtsov",
+    ingredients: {
+      about: {
+        name: "Maksym",
+        text1:
+          "Ukrainian novelist who became a war crimes investigator following the Russian invasion, and fought to uncover the true stories of her compatriots’ experiences under occupation",
+        text2: "some text2",
+        text3: "some text3",
+      },
+    },
+  },
+];
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let data;
+
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { writers: data });
 });
 
-app.post("/recipe", (req, res) => {
-  //Step 3: Write your code here to make this behave like the solution website.
-  //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+app.post("/writers", (req, res) => {
+  switch (req.body.choice) {
+    case "Viktoria":
+      data = JSON.parse(writersJSON)[0];
+      break;
+    case "Volodymyr":
+      data = JSON.parse(writersJSON)[1];
+      break;
+    case "Maksym":
+      data = JSON.parse(writersJSON)[2];
+      break;
+    default:
+      break;
+  }
+  res.redirect("/");
 });
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
-
-//my comment here 
