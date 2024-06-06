c38441c166e408f4aeef1dad1b38ceae3b26d02c
Daria Makarenko <darya.makarenko@gmail.com>
2024-06-06 23:22:26 +0300

    fix JSON parsing

--- index.js
+++ index.js
@@ -4,45 +4,8 @@ import bodyParser from "body-parser";
 const app = express();
 const port = 3000;
 
-const writersJSON = [
-  {
-    id: "0001",
-    name: "Viktoria Amelina",
-    about: {
-      name: "Viktoria",
-      text1:
-        "Ukrainian novelist who became a war crimes investigator following the Russian invasion, and fought to uncover the true stories of her compatriots’ experiences under occupation",
-      text2: "some text2",
-      text3: "some text3",
-    },
-  },
-  {
-    id: "0002",
-    name: "Volodymyr Vakulenko",
-    ingredients: {
-      about: {
-        name: "Volodymyr",
-        text1:
-          "Ukrainian novelist who became a war crimes investigator following the Russian invasion, and fought to uncover the true stories of her compatriots’ experiences under occupation",
-        text2: "some text2",
-        text3: "some text3",
-      },
-    },
-  },
-  {
-    id: "0003",
-    name: "Maksym Krivtsov",
-    ingredients: {
-      about: {
-        name: "Maksym",
-        text1:
-          "Ukrainian novelist who became a war crimes investigator following the Russian invasion, and fought to uncover the true stories of her compatriots’ experiences under occupation",
-        text2: "some text2",
-        text3: "some text3",
-      },
-    },
-  },
-];
+const recipeJSON =
+  '[{"id": "0001","type": "taco","name": "Chicken Taco","price": 2.99,"ingredients": {"protein": {"name": "Chicken","preparation": "Grilled"},  "salsa": {"name": "Tomato Salsa","spiciness": "Medium"},  "toppings": [{"name": "Lettuce",  "quantity": "1 cup",  "ingredients": ["Iceberg Lettuce"]  },      {"name": "Cheese",  "quantity": "1/2 cup",  "ingredients": ["Cheddar Cheese", "Monterey Jack Cheese"]  },      {"name": "Guacamole",  "quantity": "2 tablespoons",  "ingredients": ["Avocado", "Lime Juice", "Salt", "Onion", "Cilantro"]  },      {"name": "Sour Cream",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream"]  }      ]    }  },{"id": "0002","type": "taco","name": "Beef Taco","price": 3.49,"ingredients": {"protein": {"name": "Beef","preparation": "Seasoned and Grilled"},  "salsa": {"name": "Salsa Verde","spiciness": "Hot"},  "toppings": [{"name": "Onions",  "quantity": "1/4 cup",  "ingredients": ["White Onion", "Red Onion"]  },      {"name": "Cilantro",  "quantity": "2 tablespoons",  "ingredients": ["Fresh Cilantro"]  },      {"name": "Queso Fresco",  "quantity": "1/4 cup",  "ingredients": ["Queso Fresco"]  }      ]    }  },{"id": "0003","type": "taco","name": "Fish Taco","price": 4.99,"ingredients": {"protein": {"name": "Fish","preparation": "Battered and Fried"},  "salsa": {"name": "Chipotle Mayo","spiciness": "Mild"},  "toppings": [{"name": "Cabbage Slaw",  "quantity": "1 cup",  "ingredients": [    "Shredded Cabbage",    "Carrot",    "Mayonnaise",    "Lime Juice",    "Salt"          ]  },      {"name": "Pico de Gallo",  "quantity": "1/2 cup",  "ingredients": ["Tomato", "Onion", "Cilantro", "Lime Juice", "Salt"]  },      {"name": "Lime Crema",  "quantity": "2 tablespoons",  "ingredients": ["Sour Cream", "Lime Juice", "Salt"]  }      ]    }  }]';
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -50,19 +13,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 let data;
 
 app.get("/", (req, res) => {
-  res.render("index.ejs", { writers: data });
+  res.render("solution.ejs", { recipe: data });
 });
 
-app.post("/writers.json", (req, res) => {
+app.post("/recipe", (req, res) => {
   switch (req.body.choice) {
-    case "Viktoria":
-      data = JSON.parse(writersJSON)[0];
+    case "chicken":
+      data = JSON.parse(recipeJSON)[0];
       break;
-    case "Volodymyr":
-      data = JSON.parse(writersJSON)[1];
+    case "beef":
+      data = JSON.parse(recipeJSON)[1];
       break;
-    case "Maksym":
-      data = JSON.parse(writersJSON)[2];
+    case "fish":
+      data = JSON.parse(recipeJSON)[2];
       break;
     default:
       break;
@@ -73,3 +36,5 @@ app.post("/writers.json", (req, res) => {
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
+
